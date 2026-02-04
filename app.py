369eb2c89facf61db13e6e819f3223de7ab31f91
Mohammed rafeek <mohammedrafeek21993@gmail.com>
2026-02-04 17:02:57 +0530

    Update app.py

--- app.py
+++ app.py
@@ -6,18 +6,7 @@ def set_bg(image):
     with open(image, "rb") as f:
         encoded = base64.b64encode(f.read()).decode()
 
-    st.markdown(
-        f"""
-        <style>
-        .stApp {{
-            background-image: url("data:image/jpg;base64,{encoded}");
-            background-size: cover;
-        }}
-        </style>
-        """,
-        unsafe_allow_html=True
-    )
-def input_style():
+    def input_style():
     st.markdown("""
     <style>
     input {
@@ -34,6 +23,18 @@ def input_style():
     }
     </style>
     """, unsafe_allow_html=True)
+    
+    st.markdown(
+        f"""
+        <style>
+        .stApp {{
+            background-image: url("data:image/jpg;base64,{encoded}");
+            background-size: cover;
+        }}
+        </style>
+        """,
+        unsafe_allow_html=True
+    )
 
 
 # ---------- FILES ----------
